3a42b457c75957029a0e255519de34eb3d56d6a1
JoshuaSimon <joshsimon96@googlemail.com>
2022-05-08 18:44:32 +0200

    Initial file structure

---
+++ src/ckm.py
@@ -0,0 +1,16 @@
+# ckm.py
+#
+# Joshua Simon, 08.05.2022
+
+
+from unicodedata import name
+import numpy as np
+import pandas as pd
+
+
+def generate_data():
+    pass
+
+
+if __name__ == "main":
+    print("Done.")
